2674d6422c89eebef9e76619898bef949ebfb92f
andikhadev <andikha.dev@gmail.com>
2022-12-23 14:28:00 +0700

    feat: Updated fuel price and calculate energy

--- src/pages/EnergyCalculator/utils/fuel.ts
+++ src/pages/EnergyCalculator/utils/fuel.ts
@@ -1,52 +1,52 @@
 export const fuels = [
+  // DONE but need tons comparation
   {
     name: 'COAL',
     title: 'Coal',
     image: require('assets/img/fuels/coal.png'),
     units: ['Rupiah', 'Tons'],
-    multiplyUnit: 1000,
-    price: 4810,
+    price: 4388428,
     naturalGas: 575,
-    co2Emission: 2.42,
+    co2Emission: 2419,
   },
+  // DONE
   {
     name: 'LPG_50KG',
     title: 'LPG 50Kg',
     image: require('assets/img/fuels/lpg-50kg.png'),
     units: ['Rupiah', 'Tank'],
-    multiplyUnit: 50,
-    price: 17750,
-    naturalGas: 1265,
-    co2Emission: 3.06,
+    price: 367750,
+    naturalGas: 63.25,
+    co2Emission: 153.0,
   },
+  // DONE
   {
     name: 'LPG_BULK',
     title: 'LPG Bulk',
     image: require('assets/img/fuels/lpg-bulk.png'),
     units: ['Rupiah', 'Kg'],
-    multiplyUnit: 1,
     price: 17750,
-    naturalGas: 1265,
+    naturalGas: 1.265,
     co2Emission: 3.06,
   },
+  // DONE
   {
     name: 'HSD',
     title: 'HSD/Industrial Diesel',
     image: require('assets/img/fuels/hsd.png'),
     units: ['Rupiah', 'Liter'],
-    multiplyUnit: 1,
     price: 24450,
-    naturalGas: 1022,
+    naturalGas: 1.022,
     co2Emission: 2.68,
   },
+  // DONE
   {
     name: 'MFO',
     title: 'MFO',
     image: require('assets/img/fuels/mfo.png'),
     units: ['Rupiah', 'Liter'],
-    multiplyUnit: 1,
     price: 19500,
-    naturalGas: 1045,
+    naturalGas: 1.045,
     co2Emission: 3.2,
   },
 ]
@@ -65,6 +65,9 @@ export function calculateEnergy(
   const BASE_NATURAL_GAS_PRICE: number = 5265
   const BASE_NATURAL_GAS_CO2_EMISSION: number = 1.9
   const MMBTU_CONVERSION: number = 28.5
+  const BBTU_CONVERSION: number = 1000
+  const FUEL_CONVERSION: number = 1000
+  const NATURAL_GAS_CO2_EMISSION_CONVERSION: number = 1000
   const TAX_MULTIPLIER: number = 30000
 
   let volume: number = 0
@@ -74,8 +77,8 @@ export function calculateEnergy(
   let mmbtu: number = 0
   let bbtu: number = 0
 
-  let highSpeedDieselFuelEmission: number = 0
-  let highSpeedDieselFuelTax: number = 0
+  let fuelEmission: number = 0
+  let fuelTax: number = 0
   let naturalGasEmission: number = 0
   let naturalGasTax: number = 0
 
@@ -85,49 +88,56 @@ export function calculateEnergy(
 
   if (unit === 'Rupiah') {
     volume = Number(usageValue / target.price)
-    volumeM3 = Number(volume * target.naturalGas) / 1000
+    volumeM3 = Number(volume * target.naturalGas)
     naturalGasCosts = Number(volumeM3 * BASE_NATURAL_GAS_PRICE)
-    highSpeedDieselFuelEmission = (volume * target.co2Emission) / 1000
+    fuelEmission = (volume * target.co2Emission) / FUEL_CONVERSION
     energyCostsSavingPerMonth = usageValue - naturalGasCosts
   } else {
     volume = Number(usageValue * target.price)
-    volumeM3 = Number((usageValue * target.naturalGas) / 1000)
+    volumeM3 = Number(usageValue * target.naturalGas)
     naturalGasCosts = Number(volumeM3 * BASE_NATURAL_GAS_PRICE)
-    highSpeedDieselFuelEmission = (usageValue * target.co2Emission) / 1000
+    //   ! 1a. add fuel conversion (/ FUEL_CONVERSION) if (LPG 50KG Tank, LPG Bulk)?
+    fuelEmission = usageValue * target.co2Emission
     energyCostsSavingPerMonth = volume - naturalGasCosts
   }
 
   //   B. Penggunaan Bahan bakar eksiting Anda setara dengan penggunaan gas bumi senilai
   mmbtu = Number(volumeM3 / MMBTU_CONVERSION)
-  bbtu = Number(mmbtu / 1000)
+  bbtu = Number(mmbtu / BBTU_CONVERSION)
 
-  // console.log(volume);
+  console.log(volume)
 
-  // console.log(naturalGasCosts);
-  // console.log(volumeM3);
-  // console.log(mmbtu);
-  // console.log(bbtu);
+  console.log(naturalGasCosts)
+  console.log(volumeM3)
+  console.log(mmbtu)
+  console.log(bbtu)
 
   //   C. Jumlah Emisi Karbon & Pajak Karbon
-  naturalGasEmission = (volumeM3 * BASE_NATURAL_GAS_CO2_EMISSION) / 1000
-  highSpeedDieselFuelTax = highSpeedDieselFuelEmission * TAX_MULTIPLIER
+  naturalGasEmission =
+    (volumeM3 * BASE_NATURAL_GAS_CO2_EMISSION) /
+    NATURAL_GAS_CO2_EMISSION_CONVERSION
+  fuelTax = fuelEmission * TAX_MULTIPLIER
   naturalGasTax = naturalGasEmission * TAX_MULTIPLIER
 
-  // console.log(highSpeedDieselFuelEmission);
-  // console.log(highSpeedDieselFuelTax);
-  // console.log(naturalGasEmission);
-  // console.log(naturalGasTax);
+  // ! 1b. impacted to here
+  console.log(fuelEmission)
+  // ! 1c. impacted to here
+  console.log(fuelTax)
+  console.log(naturalGasEmission)
+  console.log(naturalGasTax)
 
   //   D. Kesimpulan
-  //   energyCostsSavingPerMonth = 11698877
-  taxSaving = highSpeedDieselFuelTax - naturalGasTax
-  co2EmissionReduction = highSpeedDieselFuelEmission - naturalGasEmission
+  taxSaving = fuelTax - naturalGasTax
+  co2EmissionReduction = fuelEmission - naturalGasEmission
   const totalSaving = Math.ceil(energyCostsSavingPerMonth + taxSaving)
 
-  // console.log(energyCostsSavingPerMonth);
-  // console.log(taxSaving);
-  // console.log(co2EmissionReduction);
-  // console.log(totalSaving);
+  console.log(energyCostsSavingPerMonth)
+  // ! 1d. impacted to here
+  console.log(taxSaving)
+  // ! 1e. impacted to here
+  console.log(co2EmissionReduction)
+  // ! 1f. impacted to here
+  console.log(totalSaving)
 
   return {
     volume,
@@ -135,8 +145,8 @@ export function calculateEnergy(
     naturalGasCosts,
     mmbtu,
     bbtu,
-    highSpeedDieselFuelEmission,
-    highSpeedDieselFuelTax,
+    fuelEmission,
+    fuelTax,
     naturalGasEmission,
     naturalGasTax,
     energyCostsSavingPerMonth,
@@ -145,3 +155,5 @@ export function calculateEnergy(
     totalSaving,
   }
 }
+
+calculateEnergy('MFO', 'Liter', 15000000)
