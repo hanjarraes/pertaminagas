06014893fe0bda156ce9efdcfbba54997b3d37c8
Andikha Dian <andikha.dev@gmail.com>
2022-12-29 09:05:37 +0700

    fix: Added z index on modal overlay

--- src/component/container/Header/index.jsx
+++ src/component/container/Header/index.jsx
@@ -1,23 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
-import { Whatsapp, HambergerMenu } from 'iconsax-react';
-import Modal from 'react-modal';
+import { Whatsapp, HambergerMenu } from 'iconsax-react'
+import Modal from 'react-modal'
 import LogoPertamina from '../../../assets/img/Logo_Pertamina_Gas_Negara.png'
 import Button from '../../Button'
 
-
 const customStyles = {
+  overlay: {
+    zIndex: 999,
+  },
   content: {
     height: window.innerHeight,
     width: window.innerWidth,
-    background: '#fff'
+    background: '#fff',
   },
-};
+}
 
 const navDefault = [
   { href: '#KelebihanGasBumi', title: 'Kelebihan Gas Bumi' },
   { href: '#FAQ', title: 'FAQ' },
-  { href: '/business', title: 'Gas Untuk Industri' }
+  { href: '/business', title: 'Gas Untuk Industri' },
 ]
 const navBusiness = [
   { href: '#CalculateSavings', title: 'Calculate Savings' },
@@ -26,57 +28,68 @@ const navBusiness = [
 ]
 
 const Header = ({ business }) => {
-  const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false)
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="/">
-          <img src={LogoPertamina} alt="Logo Header Pertamina" />
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container'>
+        <a className='navbar-brand' href='/'>
+          <img src={LogoPertamina} alt='Logo Header Pertamina' />
         </a>
-        <div className='d-flex d-md-none' onClick={() => setOpenModal(!openModal)}>
+        <div
+          className='d-flex d-md-none'
+          onClick={() => setOpenModal(!openModal)}
+        >
           <HambergerMenu />
         </div>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item" />
+        <div className='collapse navbar-collapse'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item' />
           </ul>
           <div>
-            {
-              business ?
-                (
-                  <ul className="navbar-nav nav-header-custom mr-auto d-flex align-items-center">
-
-                    {navBusiness.map((data, idx) => {
-                      return (
-                        <li className="nav-item active mx-2" key={`nav-default-${idx}`}>
-                          <a className="nav-link" href={data.href}>{data.title}</a>
-                        </li>
-                      )
-                    })}
-                    <li>
-                      <Button type='link' href="#" className='btn-light px-4' >
-                        <div className='title'>Login Sobat PGN</div>
-                      </Button>
-                    </li>
-                  </ul>
-                ) : (
-                  <ul className="navbar-nav nav-header-custom mr-auto d-flex align-items-center">
-
-                    {navDefault.map((data, idx) => {
-                      return (
-                        <li className="nav-item active mx-2" key={`nav-default-${idx}`}>
-                          <a className="nav-link" href={data.href}>{data.title}</a>
-                        </li>
-                      )
-                    })}
-                    <li>
-                      <a href="https://api.whatsapp.com/send/?phone=6281511500645&text&type=phone_number&app_absent=0" className='btn-whatsApp' >
-                        <Whatsapp /> <div className='title'>Hubungi Kami</div>
+            {business ? (
+              <ul className='navbar-nav nav-header-custom mr-auto d-flex align-items-center'>
+                {navBusiness.map((data, idx) => {
+                  return (
+                    <li
+                      className='nav-item active mx-2'
+                      key={`nav-default-${idx}`}
+                    >
+                      <a className='nav-link' href={data.href}>
+                        {data.title}
                       </a>
                     </li>
-                  </ul>
-                )
-            }
+                  )
+                })}
+                <li>
+                  <Button type='link' href='#' className='btn-light px-4'>
+                    <div className='title'>Login Sobat PGN</div>
+                  </Button>
+                </li>
+              </ul>
+            ) : (
+              <ul className='navbar-nav nav-header-custom mr-auto d-flex align-items-center'>
+                {navDefault.map((data, idx) => {
+                  return (
+                    <li
+                      className='nav-item active mx-2'
+                      key={`nav-default-${idx}`}
+                    >
+                      <a className='nav-link' href={data.href}>
+                        {data.title}
+                      </a>
+                    </li>
+                  )
+                })}
+                <li>
+                  <a
+                    href='https://api.whatsapp.com/send/?phone=6281511500645&text&type=phone_number&app_absent=0'
+                    className='btn-whatsApp'
+                  >
+                    <Whatsapp /> <div className='title'>Hubungi Kami</div>
+                  </a>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </div>
@@ -84,57 +97,69 @@ const Header = ({ business }) => {
         isOpen={openModal}
         style={customStyles}
         className='modalNavCustomHero'
-        contentLabel="Example Modal"
+        contentLabel='Example Modal'
       >
         <div>
           <div className='modalNavHeaderHero'>
-            <img src={LogoPertamina} alt="Logo Header Pertamina" />
+            <img src={LogoPertamina} alt='Logo Header Pertamina' />
             <span onClick={() => setOpenModal(!openModal)}>X</span>
           </div>
-          <div className='d-flex flex-column justify-content-between' style={{ height: window.innerHeight - 100 }}>
+          <div
+            className='d-flex flex-column justify-content-between'
+            style={{ height: window.innerHeight - 100 }}
+          >
             <div className='items-modal'>
-              {
-                business ? (
-                  navBusiness.map((data, idx) => {
+              {business
+                ? navBusiness.map((data, idx) => {
                     return (
-                      <a href={data.href} onClick={() => setOpenModal(!openModal)} key={`nav-default-${idx}-mobile`} >{data.title}</a>
+                      <a
+                        href={data.href}
+                        onClick={() => setOpenModal(!openModal)}
+                        key={`nav-default-${idx}-mobile`}
+                      >
+                        {data.title}
+                      </a>
                     )
                   })
-
-                ) : (
-                  navDefault.map((data, idx) => {
+                : navDefault.map((data, idx) => {
                     return (
-                      <a href={data.href} onClick={() => setOpenModal(!openModal)} key={`nav-default-${idx}-mobile`} >{data.title}</a>
+                      <a
+                        href={data.href}
+                        onClick={() => setOpenModal(!openModal)}
+                        key={`nav-default-${idx}-mobile`}
+                      >
+                        {data.title}
+                      </a>
                     )
-                  })
-                )
-              }
+                  })}
             </div>
-            {business ?
+            {business ? (
               <div>
-                <Button type='link' href="#" className='btn-light btn-mobile' >
+                <Button type='link' href='#' className='btn-light btn-mobile'>
                   <div className='title'>Login Sobat PGN</div>
                 </Button>
-                <Button type='link' href="#" className='btn-default mt-3' >
+                <Button type='link' href='#' className='btn-default mt-3'>
                   <div className='title'>Book a Meeting</div>
                 </Button>
               </div>
-              :
+            ) : (
               <div>
-                <a href="https://api.whatsapp.com/send/?phone=6281511500645&text&type=phone_number&app_absent=0" className='btn-whatsApp' >
+                <a
+                  href='https://api.whatsapp.com/send/?phone=6281511500645&text&type=phone_number&app_absent=0'
+                  className='btn-whatsApp'
+                >
                   <Whatsapp /> <div className='title'>Hubungi Kami</div>
                 </a>
-                <Button type='link' href="#" className='btn-default mt-3' >
+                <Button type='link' href='#' className='btn-default mt-3'>
                   <div className='title'>Daftar Sekarang</div>
                 </Button>
               </div>
-            }
-
+            )}
           </div>
         </div>
       </Modal>
-    </nav >
+    </nav>
   )
 }
 
-export default Header;
+export default Header
