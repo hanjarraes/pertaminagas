570c9d50f8f39e737b4fda58bac16b4f9ae7b389
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-03-31 15:24:23 +0700

    chore: change copies

--- src/pages/Landing/service.js
+++ src/pages/Landing/service.js
@@ -8,38 +8,45 @@ import {
 } from "iconsax-react";
 
 export const dataSeccsion = {
-  title: 'Nikmatnya hidup bebas dari rasa khawatir',
-  desc: 'Mulai dari ketersediaan gas bumi yang aman hingga cara penggunaan yang mudah, GasKita siap jadikan hidupmu lebih praktis',
-  itemSeccsion :[
-  {
-    logo: <ShieldTick />,
-    title: "Melindungi Anda",
-    decs: "Lebih aman dengan asuransi kerugian hingga Rp30 Juta dan layanan teknis 24 jam melalui Contact Center dan Tim Lapangan",
-  },
-  {
-    logo: <Verify />,
-    title: "Lebih Modern",
-    decs: "Minimalis, cukup dengan pipa seperti di Eropa",
-  },
-  {
-    logo: <Like1 />,
-    title: "Paling Praktis",
-    decs: "Cara pakainya, cukup putar keran gas. Cara bayarnya, cukup dari ponsel saja",
-  },
-  {
-    logo: <StatusUp />,
-    title: "Selalu Tersedia",
-    decs: "24 Jam / 7 Hari, baik aliran gasnya maupun tim teknis yang selalu siap membantu Anda",
-  },
-  {
-    logo: <Setting4 />,
-    title: "Fleksibel",
-    decs: "Bisa digunakan sesuai kebutuhan Anda, mulai dari masak hingga usaha kecil",
-  },
-  {
-    logo: <Gift />,
-    title: "Banyak Bonusnya",
+  title: "Nikmati hidup bebas dari rasa khawatir",
+  desc:
+    "Mulai dari ketersediaan gas bumi yang aman hingga cara penggunaan yang mudah, GasKita siap jadikan hidupmu lebih praktis",
+  itemSeccsion: [
+    {
+      logo: <ShieldTick />,
+      title: "Melindungi Anda",
+      decs:
+        "Lebih aman dengan asuransi kerugian hingga Rp30 Juta dan layanan teknis 24 jam melalui Contact Center dan Tim Lapangan",
+    },
+    {
+      logo: <Verify />,
+      title: "Lebih Modern",
+      decs:
+        "Merupakan bagian dari standar hidup modern di negara maju seperti Eropa",
+    },
+    {
+      logo: <Like1 />,
+      title: "Paling Praktis",
+      decs:
+        "Cara pakainya, cukup putar keran gas.\n Cara bayarnya, cukup dari ponsel saja",
+    },
+    {
+      logo: <StatusUp />,
+      title: "Selalu Tersedia",
+      decs:
+        "24 Jam / 7 Hari, baik aliran gasnya maupun tim teknis yang selalu siap membantu Anda",
+    },
+    {
+      logo: <Setting4 />,
+      title: "Fleksibel",
+      decs:
+        "Bisa digunakan sesuai kebutuhan Anda, mulai dari masak hingga usaha kecil",
+    },
+    {
+      logo: <Gift />,
+      title: "Banyak Bonusnya",
       decs:
         "Pipa Instalasi (s.d 15 m); Konversi kompor; Pipe safety inspection",
-  },
-]};
+    },
+  ],
+};
